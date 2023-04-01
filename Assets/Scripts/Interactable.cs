e3e9edb24e135929e80703002a5a855c889b7bbf
Boy-Bird <yashparihar44@gmail.com>
2023-04-01 10:18:27 +0530

    Day3: HealthBar

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    public bool useEvents;
     // message displayed to player when looking at an interactable.
     public string promptMessage;
     public void BaseInteract()
     {
+        if (useEvents)
+            GetComponent<InteractionEvent>().OnInteract.Invoke();
         Interact();
     }
     protected virtual void Interact()
